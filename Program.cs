d655b22ef196bb4f25f0ab0947dd2e60a381f032
Lcuky12 <118757873+Lcuky12@users.noreply.github.com>
2023-03-18 15:35:44 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp65
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = { { 1, 2, 3, }, { 4, 5, 6, }, { 5, 6, 7, } };
+
+            int sum = 0;
+            int multiplication = 1;
+            int indexZero = 0;
+            int indexOne = 1;
+
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                multiplication *= numbers[indexZero, i];
+            }
+
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                sum += numbers[j, indexOne];
+            }
+
+            Console.WriteLine(" Сумма второй строки - " + sum);
+            Console.WriteLine(" Произведение первого столбца - " + multiplication);
+        }
+    }
+}
